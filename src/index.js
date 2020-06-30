c005729ef88ba90b5f88d8bb161bc4fe09a22566
Matthias Klan <matthias.klan@gmail.com>
2020-06-30 02:53:49 +0200

    styling

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import createGame from './game';
 import './styles.css';
 
+function renderHighScore(value) {
+  document.getElementById('highScore').innerHTML = `<label>best:</label> ${value}`;
+}
+
+function renderLastScore(value) {
+  document.getElementById('lastScore').innerHTML = `<label>last:</label> ${value}`;
+}
+
+
 
 function handleGameOver({ total, isHighcore }) {
-  document.getElementById('lastScore').innerText = `last: ${total}`;
+  renderLastScore(total);
   if (isHighcore) {
     // Store
     localStorage.setItem("highscore", total);
-    document.getElementById('highScore').innerText = `best: ${total}`;
+    renderHighScore(total);
   }
 }
 
 const highscore = localStorage.getItem('highscore');
 if(highscore){
-  document.getElementById('highScore').innerText = `best: ${highscore}`;
+  renderHighScore(highscore);
 }
 
 
