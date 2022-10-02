494431ea93cdfd845894bc2a42276ef8eff9be83
Matthias Klan <matthias.klan@gmail.com>
2022-10-02 22:14:01 +0200

    formatting

--- src/index.js
+++ src/index.js
@@ -2,29 +2,31 @@ import createGame from './game';
 import './styles.css';
 
 function renderHighScore(value) {
-  document.getElementById('highScore').innerHTML = `<label>best:</label> ${value}`;
+  document.getElementById(
+    'highScore',
+  ).innerHTML = `<label>best:</label> ${value}`;
 }
 
 function renderLastScore(value) {
-  document.getElementById('lastScore').innerHTML = `<label>last:</label> ${value}`;
+  document.getElementById(
+    'lastScore',
+  ).innerHTML = `<label>last:</label> ${value}`;
 }
 
-
-
 function handleGameOver({ total, isHighcore }) {
+  console.log('game over', total);
   renderLastScore(total);
   if (isHighcore) {
     // Store
-    localStorage.setItem("highscore", total);
+    localStorage.setItem('highscore', total);
     renderHighScore(total);
   }
 }
 
 const highscore = localStorage.getItem('highscore');
-if(highscore){
+if (highscore) {
   renderHighScore(highscore);
 }
-
 
 createGame({
   canvasEl: 'canvas',
