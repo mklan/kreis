d4119eb9336dd2188d9dc0924bbd2cff7d0712cc
Matthias Klan <matthias.klan@gmail.com>
2022-10-02 22:13:56 +0200

    Fix game over

--- src/game.js
+++ src/game.js
@@ -86,7 +86,7 @@ function createGame({ canvasEl, onGameOver, highscore }) {
     }
 
     onGameOver({
-      total: total.parseInt(),
+      total: parseInt(total, 10),
       avg: (10000 - total) / pointCount,
       isHighcore: highscore === total,
     });
